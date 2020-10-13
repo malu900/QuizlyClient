03587d4fb1a0c747c5bb26b01c68286ec677debd
I401589 <malu.vandenberg@student.fontys.nl>
2020-10-13 11:14:05 +0200

    working auth

--- src/auth/Register.js
+++ src/auth/Register.js
@@ -5,26 +5,27 @@ import { Container, Form, Button } from "react-bootstrap";
 export class Register extends Component {
   render() {
     return (
-      //   <Container id="register">
-      //     <h2> OVERLAY! </h2>
-      //   </Container>
       <Container id="register">
         <div className="auth">
           <h2> Register </h2>
           <Form>
             <Form.Group controlId="formBasicEmail">
+              <Form.Label>Name</Form.Label>
+              <Form.Control type="name" placeholder="Enter email" />
+              <Form.Text className="text-muted"></Form.Text>
+            </Form.Group>
+            <Form.Group controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
               <Form.Control type="email" placeholder="Enter email" />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
+              <Form.Text className="text-muted"></Form.Text>
             </Form.Group>
             <Form.Group controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
               <Form.Control type="password" placeholder="Password" />
             </Form.Group>
-            <Form.Group controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Check me out" />
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
             </Form.Group>
             <Button variant="primary" type="submit">
               Submit
