6dd18af67a744a422e65c1c82ccd48f0fb24f81b
I401589 <malu.vandenberg@student.fontys.nl>
2020-10-27 09:19:51 +0100

    test

--- src/Quiz/AllQuiz.js
+++ src/Quiz/AllQuiz.js
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
 
 export class AllQuiz extends Component {
+  constructor(props) {
+    super(props);
+    this.setState({
+      Quiz: [],
+    });
+  }
   render() {
     return <div></div>;
   }
