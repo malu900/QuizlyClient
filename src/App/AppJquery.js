29966e2ab2e8ef4f33642420f0630418b954eb3f
I401589 <malu.vandenberg@student.fontys.nl>
2020-12-15 11:44:51 +0100

    only use colors where white text is visibile

--- src/App/AppJquery.js
+++ src/App/AppJquery.js
@@ -1,61 +1,17 @@
 import $ from "jquery";
 
-// var colours = ["blue", "orange", "pink"];
-// var counter = 0;
-// function cycleBackground() {
-//   console.log("test");
-//   $(".animate-bg").animate({ backgroundColor: colours[counter] }, 500);
-//   counter++;
-//   if (counter == colours.length) {
-//     counter = 0;
-//   }
-// }
-// cycleBackground();
-// $(".animate-bg").css("backgroundColor:", "pink");
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function getGMapRandomColor() {
+  return "hsla(" + Math.floor(Math.random() * 360) + ", 100%, 70%, 1)";
+}
 var arr = ["#f00", "#0f0", "#00f"];
 function changeColor() {
   $(".animate-bg").css({
-    "background-color":
-      "rgba(" +
-      Math.floor(Math.random() * 255) +
-      1 +
-      "," +
-      Math.floor(Math.random() * 255) +
-      1 +
-      "," +
-      Math.floor(Math.random() * 255) +
-      1 +
-      ", 0.7)",
-    transition: "all 8s",
+    backgroundColor: getGMapRandomColor(),
+    transition: "all 5s",
   });
 }
 changeColor();
 setInterval(changeColor, 5000);
-// setInterval(cycleBackground, 10000);
-
-// // $('.rounded-circle').c() {
-
-// // }
-// $(function () {
-//   function unifyHeights() {
-//     var maxHeight = 0;
-//     $("#content-container")
-//       .children("#center-content")
-//       .each(function () {
-//         var height = $(this).outerHeight();
-//         // alert(height);
-//         if (height > maxHeight) {
-//           maxHeight = height;
-//         }
-//       });
-//     $("#navigation, #content").css("height", maxHeight);
-//   }
-//   unifyHeights();
-// });
-// function expandToWindow(element) {
-//   var margin = 10;
-
-//   if (element.style.height < window.innerHeight) {
-//     element.style.height = window.innerHeight - 2 * margin;
-//   }
-// }
